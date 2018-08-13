0489577a1b70e01e08fd3f76dcb2483ed2db22d9
WalsjeWouter <csource@live.nl>
2018-08-13 15:11:18 +0200

    Added new VS solution and empty AzureFunctions project.

---
+++ MailSender/Send.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace MailSender
+{
+    public static class Send
+    {
+        [FunctionName("Send")]
+        public static void Run([QueueTrigger("mailqueue", Connection = "UseDevelopmentStorage=true")]string queueItem, TraceWriter log)
+        {
+            log.Info($"C# Queue trigger function processed: {queueItem}");
+        }
+    }
+}
